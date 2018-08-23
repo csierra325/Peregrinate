3a480fc4554354d31ddfaf3f4a39dc04c7baa8bf
Christina Gerard <ChristinaGerard@Christinas-MacBook-Pro.local>
2018-08-23 15:32:55 -0400

    login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import axios from "axios";
+// import axios from "axios";
+import API from "../utils/API";
 
 class Login extends Component {
     // Setting the initial values of this.state.username and this.state.password
@@ -9,6 +10,17 @@ class Login extends Component {
         new_username: "",
         new_password_one: "",
         new_password_two: ""
+    };
+    // componentDidMount() {
+    //     this.addUser();
+    // }
+
+    addUser = () => {
+        API.saveUser({username: this.state.new_username, password: this.state.new_password_one})
+            .then(res =>
+               console.log(res.data)
+            )
+            .catch(err => console.log(err))
     };
 
     // handle any changes to the input fields
@@ -32,20 +44,15 @@ class Login extends Component {
     handleFormSubmitNewUser = event => {
         event.preventDefault();
         console.log(`New User\n----------------\nUsername: ${this.state.new_username}\nPassword One: ${this.state.new_password_one} \nPassword Two:${this.state.new_password_two}\n--------------`);
-        this.setState({ new_username: "", new_password_one: "", new_password_two: "" });
+        // this.setState({ new_username: "", new_password_one: "", new_password_two: "" });
         if (this.state.new_password_one === this.state.new_password_two) {
             alert(`Passwords match!`);
-            // axios.post('/Login', {
-            //     new_username: this.state.new_username,
-            //     password: this.state.new_password_two
-            // });
+            this.addUser();
+
         } else {
             alert(`Passwords don't match`);
         }
-
-
     }
-
 
     render() {
         return (
