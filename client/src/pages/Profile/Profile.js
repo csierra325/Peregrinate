618f1f4e9e7754fc74ac010e8dc64a6cb5597be2
Cameron Ashlyn <33036729+CameronAshlyn@users.noreply.github.com>
2018-08-28 13:51:52 -0400

    syncing with master

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,16 +1,13 @@
 import React, { Component } from "react";
 
 import Jumbotron from "../../components/Jumbotron";
-<<<<<<< HEAD
 // import Form from "../../components/Form";
 import Greeting from "../../components/Greeting";
 import ProfileCard from "../../components/ProfileCard";
 import ProfileForm from "../../components/ProfileForm";
-=======
 import Form from "../../components/Form";
 import Login from '../Login';
 import NavTabs from "../../components/NavTabs"
->>>>>>> c58788bc2cacb44eebf088141065b6844279d332
 
 class Profile extends Component {
   state = {
@@ -20,11 +17,8 @@ class Profile extends Component {
 
   render() {
     return (
-<<<<<<< HEAD
-=======
       <div className = "wrapper">
         <NavTabs />
->>>>>>> c58788bc2cacb44eebf088141065b6844279d332
       <Jumbotron>
         <Greeting />
         
@@ -41,8 +35,10 @@ class Profile extends Component {
               
             
           </div>
+         
         
       </Jumbotron>
+      </div> 
     );
   }
 }
