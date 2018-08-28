f324fc8f5a8a46aa4d1f71653892d4a21deaa0fc
Camille Sierra <camillemsierra@gmail.com>
2018-08-28 11:12:20 -0400

    sc updates

--- client/src/pages/Friends/Friends.js
+++ client/src/pages/Friends/Friends.js
@@ -1,12 +1,6 @@
 import React, { Component } from "react";
 import Jumbotron from "../../components/Jumbotron";
-<<<<<<< HEAD
 import NavTabs from "../../components/NavTabs"
-=======
-
-// console.log(this.props.history.location.pathname);
-
->>>>>>> 97659e4e28c028503b21bc9c91cad4dcc9a7be5e
 
 class Friends extends Component {
 
@@ -26,11 +20,7 @@ class Friends extends Component {
   render() {
     return (
       <div className="wrapper">
-<<<<<<< HEAD
         <NavTabs />
-=======
-
->>>>>>> 97659e4e28c028503b21bc9c91cad4dcc9a7be5e
         <Jumbotron>
           <h2><strong>{this.state.username}</strong></h2>
           <h1>I'm the friends page!</h1>
