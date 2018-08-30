6f33237da4d732b9de8ba5caa3772fb3da5414db
Cameron Ashlyn <33036729+CameronAshlyn@users.noreply.github.com>
2018-08-30 16:32:52 -0400

    first pass at profile image render

--- client/src/components/ProfileCard/ProfileCard.js
+++ client/src/components/ProfileCard/ProfileCard.js
@@ -1,82 +1,121 @@
 import React, { Component } from "react";
-import request from 'superagent';
-import Dropzone from "react-dropzone";
-
-//import statements
-const CLOUDINARY_UPLOAD_PRESET = "peregrinate";
-const CLOUDINARY_UPLOAD_URL =
-  "https://api.cloudinary.com/v1_1/peregrinate/upload";
-
-
-class ProfileCard extends Component {
-  // Setting the initial values of this.state.username and this.state.password
-  state = {
-    uploadedFileCloudinaryUrl: ""
-  };
-
-  onImageDrop(files) {
-    this.setState({
-      uploadedFile: files[0]
-    });
-
-    this.handleImageUpload(files[0]);
-  }
-
-  handleImageUpload(file) {
-    let upload = request
-      .post(CLOUDINARY_UPLOAD_URL)
-      .field("upload_preset", CLOUDINARY_UPLOAD_PRESET)
-      .field("file", file);
-
-    upload.end((err, response) => {
-      if (err) {
-        console.error(err);
-      }
-
-      if (response.body.secure_url !== "") {
-        this.setState({
-          uploadedFileCloudinaryUrl: response.body.secure_url
-        });
-      }
-    });
-  }
+import {Image} from 'cloudinary-react';
 
 
 
 
 
 
-  
 
-  render() {
-    return (
-      <div >
-      <form>
-        <div className="FileUpload">
-          <Dropzone
-            onDrop={this.onImageDrop.bind(this)}
-            multiple={false}
-            accept="image/*"
-          >
-            <div>Drop an image or click to select a file to upload.</div>
-          </Dropzone>
-        </div>
 
-        <div>
-          {this.state.uploadedFileCloudinaryUrl === "" ? null : (
-            <div>
-              <p>{this.state.uploadedFile.name}</p>
-              <img 
-                src={this.state.uploadedFileCloudinaryUrl}
-                alt="Profile"
-                 />
-            </div>
-          )}
-        </div>
-      </form>
-      </div>
-    );
-  }
-}
 
-export default ProfileCard;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Below is working code for rendering image in browser using <dropzone>
+/////////////////////////////////////////////////////////////////////////////
+
+// import React, { Component } from "react";
+// import request from "superagent";
+// import Dropzone from "react-dropzone";
+
+// //import statements
+// const CLOUDINARY_UPLOAD_PRESET = "ciqbkaar";
+// const CLOUDINARY_UPLOAD_URL =
+//   "https://api.cloudinary.com/v1_1/peregrinate/upload";
+
+// const dropzoneStyle = {
+//   position: "relative",
+//   width: "200px",
+//   height: "200px",
+//   border: "2px dashed rgb(102, 102, 102)",
+//   borderRadius: "5px"
+// };
+
+// const profileStyle = {
+//   position: "relative",
+//   width: "300px",
+//   height: "300px",
+//   border: "2px solid rgb(102, 102, 102)",
+//   borderRadius: "5px",
+//   zIndex: "2"
+// };
+
+// class ProfileCard extends Component {
+//   // Setting the initial values of this.state.username and this.state.password
+//   state = {
+//     uploadedFileCloudinaryUrl: ""
+//   };
+
+//   onImageDrop(files) {
+//     this.setState({
+//       uploadedFile: files[0]
+//     });
+
+//     this.handleImageUpload(files[0]);
+//   }
+
+//   handleImageUpload(file) {
+//     let upload = request
+//       .post(CLOUDINARY_UPLOAD_URL)
+//       .field("upload_preset", CLOUDINARY_UPLOAD_PRESET)
+//       .field("file", file);
+
+//     upload.end((err, response) => {
+//       if (err) {
+//         console.error(err);
+//       }
+
+//       if (response.body.secure_url !== "") {
+//         this.setState({
+//           uploadedFileCloudinaryUrl: response.body.secure_url
+//         });
+//       }
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <form>
+//           <div className="FileUpload">
+//             <Dropzone
+//               onDrop={this.onImageDrop.bind(this)}
+//               multiple={false}
+//               accept="image/*"
+//               style={dropzoneStyle}
+//             >
+//               <div className="profile">
+//                 Drop an image or click to select a file to upload.
+//               </div>
+//               <div>
+//                 {this.state.uploadedFileCloudinaryUrl === "" ? null : (
+//                   <div>
+//                     {/* <p>{this.state.uploadedFile.name}</p> */}
+//                     <img
+//                       src={this.state.uploadedFileCloudinaryUrl}
+//                       alt="Profile"
+//                       style={profileStyle}
+//                     />
+//                   </div>
+//                 )}
+//               </div>
+//             </Dropzone>
+//           </div>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+
+// export default ProfileCard;
