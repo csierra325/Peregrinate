ffb426fbe34fdae440edd01336e140c259c81d2c
Sabrina Carbo <sabrinacarbo@Sabrinas-MacBook-Pro.local>
2018-09-05 13:11:27 -0400

    added us states seeds

--- scripts/seedUSstates.js
+++ scripts/seedUSstates.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const db = require("../models");
-const citiesCSV = require("./cities.json");
+const citiesCSV = require("./usstates.json");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/peregrinate");
 
