56f48cbc718fbc62a2615fc672e397d4acf26ace
Sabrina Carbo <sabrinacarbo@sabrinas-mbp.attlocal.net>
2018-09-07 07:07:22 -0400

    fixed gallery on travel list page

--- client/src/components/Gallery/Gallery.js
+++ client/src/components/Gallery/Gallery.js
@@ -2,81 +2,90 @@ import React from "react";
 import "./Gallery.css"
 
 const Gallery = () => (
- <div className="Gallery">
- <div className = "flexGallery">
- <div className="img-container" >
- <div className="column">
- <div className="content">
-    <img className="GalleryImage" src="https://www.usnews.com/dims4/USNEWS/41a481b/2147483647/resize/1200x%3E/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2Fd3%2Fb9%2F062ecaa94fff952db4662efbd809%2F30.%202-Yellowstone-Getty.jpg"   alt="Yellowstone Getty"/>
-    <p>Yellowstone National Park</p>
-    <p>Travelers will be enchanted by the colors of Yellowstone.
-  The Grand Prismatic Spring's waters are reminiscent of a rainbow, Yellowstone
-   Lake is a deep blue and framed by green trees and snowcapped mountains.</p>
-    </div>
-    </div>
-    <div className="column">
-    <div className="content">
-    <img className="GalleryImage" src="https://ytravelblog-nabbqugz.netdna-ssl.com/wp-content/uploads/2016/08/Paris-France-Europe.jpg" alt="Paris France"/>
-    <p>Paris, France</p>
-    <p>is the pinnacle of art, history, culture and tourism. The City of Light is home to the expansive Louvre art museum, the magical Eiffel Tower and the charming Champs-Élysées lined with high-end stores and boutiques.</p>
-    </div>
-    </div>
-    <div className="column">
-    <div className="content">
-    <img className="GalleryImage" src="https://www.usnews.com/dims4/USNEWS/22b9bd9/2147483647/resize/1200x%3E/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2F4b%2Fb5%2F987d142c4c8ca103177ab7d057a1%2F29-3.%20Rome-Getty.jpg" alt="Rome Getty"/>
-    <p>Rome, Italy</p>
-    <p>Delicious food (pizza, pasta, gelato, you name it), rich coffee and excellent wine are a few reasons why travelers love Rome. However, food and drink aren't the only things this city has to offer. You'll find major highlights of art and history.</p>
-    </div>
-    </div>
-    <div className="column">
-    <div className="content">
-    <img className="GalleryImage" src="https://www.usnews.com/dims4/USNEWS/1d1e666/2147483647/resize/1200x%3E/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2Fa0%2F53%2F600ab27b4832be681e1494a035ea%2F28.%20-4.%20Tahiti%2C%20French%20Polynesia-Getty.jpg" alt="Polynesia Getty" />
-    <p>Tahiti, French Polynesia</p>
-    <p>Tranquil Tahiti offers a little bit of everything. This island nation in French Polynesia is home to incredible beaches, stunning natural wonders like waterfalls, and interesting museums that teach visitors about the history of Tahiti.</p>
-    </div>
-    </div>
-    <div className="column">
-    <div className="content">
-    <img className="GalleryImage" src="https://www.usnews.com/dims4/USNEWS/42f9f35/2147483647/resize/1200x%3E/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2F5c%2Fbb%2Ff251ce4444a5b15735409d480ad0%2F27-5.%20London-Getty.jpg" alt="London Getty"/>
-    <p>London, England</p>
-    <p>Home to a wealth of cultural and historical institutions, family-friendly attractions,
-         plus a lively nightlife scene, London appeals to travelers young and old. 
-         Must-see attractions include the British Museum and Buckingham Palace and much more.</p>
-    </div>
-    </div>
-    <div className="column">
-    <div className="content">
-    <img className="GalleryImage" src="https://www.usnews.com/dims4/USNEWS/46239f4/2147483647/resize/1200x%3E/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2Ffb%2Fef%2Fc12360464b8b95b40037301de130%2F26.%206-South%20Island-Getty.jpg" alt="Island Getty" />
-    <p>South Island, New Zealand</p>
-    <p>It's the breathtaking natural landscape that woos adventurers to New Zealand's South Island, where outdoor 
-        opportunities abound. Visitors explore the area's glaciers, watch for wildlife, tour the region's wineries and stargaze on top of Mount Cook.</p>
-    </div>
-    </div>
-    <div className="column">
-    <div className="content">
-    <img className="GalleryImage" src="https://www.usnews.com/dims4/USNEWS/5e52ebc/2147483647/resize/1200x%3E/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2F14%2F98%2F633d1ec647a2a60f71f7dcfeb201%2F25.%207-Phuket-Getty.jpg" alt="Phuket Getty" />
-    <p> Phuket, Thailand</p>
-    <p>Phuket attracts travelers looking for a taste of exotic Thailand. The Andaman Sea's aquamarine-tinted water provides a striking contrast to the stark white sand beaches and emerald jungles that line the coast.</p>
-    </div>
-    </div>
-    <div className="column">
-    <div className="content">
-    <img className="GalleryImage" src="https://www.usnews.com/dims4/USNEWS/db9038f/2147483647/resize/1200x%3E/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2Fad%2F2a%2F43d7bcaf456e82efeb2ce7f27d00%2F24-8-grand-canyon-getty.jpg" alt="Grand Canyon Getty"/>
-    <p>Grand Canyon National Park, Arizona</p>
-    <p>The Grand Canyon is indeed grand, with its colorful rock formations stretching across 277 river miles.
-         One of America's most popular national parks (it attracts around 6 million visitors annually),
-          it offers ample opportunities for outdoor fun.</p>
-    </div>
-    </div>
-    <div className="column">
-    <div className="content">
-    <img className="GalleryImage" src="https://www.usnews.com/dims4/USNEWS/02bce06/2147483647/resize/1200x%3E/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2F4b%2F22%2Fa229ca3f4736bf22645322a359de%2F23.%209-Dubai-Getty.jpg" alt="Dubai Getty"/>
-    <p>Dubai, United Arab Emirates</p>
-    <p>Dubai is home to the world's tallest building, the Burj Khalifa, one of the world's largest shopping centers, the Dubai Mall, and one of the only indoor ski resorts in the world, Ski Dubai.</p>
-    </div>
-    </div>
-</div>
-</div>
+<div className="Gallery">
+        <div className="row">
+            <div className="col-sm-4">
+                <div className="content">
+                    <img className="GalleryImage" src="https://www.usnews.com/dims4/USNEWS/41a481b/2147483647/resize/1200x%3E/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2Fd3%2Fb9%2F062ecaa94fff952db4662efbd809%2F30.%202-Yellowstone-Getty.jpg"   alt="Yellowstone Getty"/>
+                    <p>Yellowstone National Park</p>
+                    <p>Travelers will be enchanted by the colors of Yellowstone.
+                    The Grand Prismatic Spring's waters are reminiscent of a rainbow, Yellowstone
+                    Lake is a deep blue and framed by green trees and snowcapped mountains.</p>
+                </div>
+            </div>
+
+            <div className="col-sm-4">
+                <div className="content">
+                    <img className="GalleryImage" src="https://ytravelblog-nabbqugz.netdna-ssl.com/wp-content/uploads/2016/08/Paris-France-Europe.jpg" alt="Paris France"/>
+                    <p>Paris, France</p>
+                    <p>is the pinnacle of art, history, culture and tourism. The City of Light is home to the expansive Louvre art museum, the magical Eiffel Tower and the charming Champs-Élysées lined with high-end stores and boutiques.</p>
+                </div>
+            </div>
+
+            <div className="col-sm-4">
+                <div className="content">
+                    <img className="GalleryImage" src="https://www.usnews.com/dims4/USNEWS/22b9bd9/2147483647/resize/1200x%3E/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2F4b%2Fb5%2F987d142c4c8ca103177ab7d057a1%2F29-3.%20Rome-Getty.jpg" alt="Rome Getty"/>
+                    <p>Rome, Italy</p>
+                    <p>Delicious food (pizza, pasta, gelato, you name it), rich coffee and excellent wine are a few reasons why travelers love Rome. However, food and drink aren't the only things this city has to offer. You'll find major highlights of art and history.</p>
+                </div>
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="col-sm-4">
+                <div className="content">
+                    <img className="GalleryImage" src="https://www.usnews.com/dims4/USNEWS/1d1e666/2147483647/resize/1200x%3E/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2Fa0%2F53%2F600ab27b4832be681e1494a035ea%2F28.%20-4.%20Tahiti%2C%20French%20Polynesia-Getty.jpg" alt="Polynesia Getty" />
+                    <p>Tahiti, French Polynesia</p>
+                    <p>Tranquil Tahiti offers a little bit of everything. This island nation in French Polynesia is home to incredible beaches, stunning natural wonders like waterfalls, and interesting museums that teach visitors about the history of Tahiti.</p>
+                </div>
+            </div>
+
+            <div className="col-sm-4">
+                <div className="content">
+                    <img className="GalleryImage" src="https://www.usnews.com/dims4/USNEWS/42f9f35/2147483647/resize/1200x%3E/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2F5c%2Fbb%2Ff251ce4444a5b15735409d480ad0%2F27-5.%20London-Getty.jpg" alt="London Getty"/>
+                    <p>London, England</p>
+                    <p>Home to a wealth of cultural and historical institutions, family-friendly attractions,
+                        plus a lively nightlife scene, London appeals to travelers young and old. 
+                    Must-see attractions include the British Museum and Buckingham Palace and much more.</p>
+                </div>
+            </div>
+
+             <div className="col-sm-4">
+                <div className="content">
+                    <img className="GalleryImage" src="https://www.usnews.com/dims4/USNEWS/46239f4/2147483647/resize/1200x%3E/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2Ffb%2Fef%2Fc12360464b8b95b40037301de130%2F26.%206-South%20Island-Getty.jpg" alt="Island Getty" />
+                    <p>South Island, New Zealand</p>
+                    <p>It's the breathtaking natural landscape that woos adventurers to New Zealand's South Island, where outdoor 
+                    opportunities abound. Visitors explore the area's glaciers, watch for wildlife, tour the region's wineries and stargaze on top of Mount Cook.</p>
+                </div>
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="col-sm-4">
+                <div className="content">
+                    <img className="GalleryImage" src="https://www.usnews.com/dims4/USNEWS/5e52ebc/2147483647/resize/1200x%3E/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2F14%2F98%2F633d1ec647a2a60f71f7dcfeb201%2F25.%207-Phuket-Getty.jpg" alt="Phuket Getty" />
+                    <p> Phuket, Thailand</p>
+                    <p>Phuket attracts travelers looking for a taste of exotic Thailand. The Andaman Sea's aquamarine-tinted water provides a striking contrast to the stark white sand beaches and emerald jungles that line the coast.</p>
+                </div>
+            </div>
+
+            <div className="col-sm-4">
+                <div className="content">
+                    <img className="GalleryImage" src="https://www.usnews.com/dims4/USNEWS/db9038f/2147483647/resize/1200x%3E/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2Fad%2F2a%2F43d7bcaf456e82efeb2ce7f27d00%2F24-8-grand-canyon-getty.jpg" alt="Grand Canyon Getty"/>
+                    <p>Grand Canyon National Park, Arizona</p>
+                    <p>The Grand Canyon is indeed grand, with its colorful rock formations stretching across 277 river miles.
+                    One of America's most popular national parks (it attracts around 6 million visitors annually),
+                    it offers ample opportunities for outdoor fun.</p>
+                </div>
+            </div>
+            <div className="col-sm-4">
+                <div className="content">
+                    <img className="GalleryImage" src="https://www.usnews.com/dims4/USNEWS/02bce06/2147483647/resize/1200x%3E/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2F4b%2F22%2Fa229ca3f4736bf22645322a359de%2F23.%209-Dubai-Getty.jpg" alt="Dubai Getty"/>
+                    <p>Dubai, United Arab Emirates</p>
+                    <p>Dubai is home to the world's tallest building, the Burj Khalifa, one of the world's largest shopping centers, the Dubai Mall, and one of the only indoor ski resorts in the world, Ski Dubai.</p>
+                </div>
+            </div>
+        </div>
 </div>
 );
 
---
+++ client/src/components/TravelIdeasJumbotron/TravelIdeasJumbotron.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TravelIdeasJumbotron = ({ children }) => (
+  <div
+    style={{ height: 1300, clear: "both", textAlign: "center" }}
+    className="jumbotron"
+  >
+    {children}
+  </div>
+);
+
+export default TravelIdeasJumbotron;
